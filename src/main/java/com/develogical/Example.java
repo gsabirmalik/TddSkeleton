f09bc5e5f0eb62121bafc0c7cc005f05f1f5ae30
gsabirmalik <gsabirmalik@hotmail.com>
2016-07-12 11:37:29 +0100

    Exercise Done...

--- src/main/java/com/develogical/Example.java
+++ src/main/java/com/develogical/Example.java
@@ -1,9 +1,41 @@
 package com.develogical;
 
+import java.util.ArrayList;
+
 public class Example {
 
-	public int answer() {
-		return 42;	
+	public ArrayList<Integer> RecentlyUsedItems = null;
+
+	public Example(){
+		RecentlyUsedItems = new ArrayList<Integer>();
 	}
 
+	public int ListSize()
+	{
+		return RecentlyUsedItems.size();
+	}
+
+	public int AddItem(int item){
+
+		Integer existingIndex = RecentlyUsedItems.lastIndexOf(item);
+		if(existingIndex == 0)
+			return RecentlyUsedItems.size();
+
+		if(existingIndex>0) {
+			RecentlyUsedItems.remove(existingIndex);
+			RecentlyUsedItems.add(0, item);
+		}
+		else
+		RecentlyUsedItems.add(0, item);
+
+		return RecentlyUsedItems.size();
+	}
+
+	public Integer RetrieveItem(){
+
+		if(RecentlyUsedItems.size() == 0)
+			throw new IllegalStateException("List is Empty");
+
+		return RecentlyUsedItems.remove(0);
+	}
 }
--- src/test/java/com/develogical/ExampleTest.java
+++ src/test/java/com/develogical/ExampleTest.java
@@ -2,14 +2,75 @@ package com.develogical;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.fail;
 
 public class ExampleTest {
 
-	@Test 
-	public void canAnswerTheUniversalQuestion() {
-		assertThat(new Example().answer(), is(42));
+//	@beforeClass
+//	public void ClassInitialzie(){
+//
+//	}
+//
+//	@before
+//	void TestInitialize(){
+//
+//	}
+
+	@Test
+	public void ListShouldBeEmptyWhenInitialized() {
+
+		assertThat(new Example().ListSize(), is(0));
 	}
 
+	@Test
+	public void AddNewItemShouldGiveNewSizeOfTheList() {
+
+		Example recentlyUsedList = new Example();
+		assertThat(recentlyUsedList.AddItem(1), is(1));
+		assertThat(recentlyUsedList.AddItem(3), is(2));
+		assertThat(recentlyUsedList.AddItem(5), is(3));
+		assertThat(recentlyUsedList.AddItem(7), is(4));
+	}
+
+	@Test
+	public void AddingAnExisitngItemShouldBringExistingToTheFront() {
+
+		Example recentlyUsedList = new Example();
+		recentlyUsedList.AddItem(1);
+		recentlyUsedList.AddItem(3);
+		recentlyUsedList.AddItem(5);
+		recentlyUsedList.AddItem(3);
+
+		assertThat(recentlyUsedList.RecentlyUsedItems.get(0), is(3));
+		assertThat(recentlyUsedList.ListSize(), is(3));
+	}
+
+	@Test
+	public void RetrieveShouldReturnLastAddedItem()
+	{
+		Example recentlyUsedList = new Example();
+		recentlyUsedList.AddItem(1);
+		recentlyUsedList.AddItem(3);
+
+		assertThat(recentlyUsedList.RetrieveItem(), is(3));
+		assertThat(recentlyUsedList.RetrieveItem(), is(1));
+	}
+
+	@Test
+	public void RetrieveOnEmptyListShouldRaiseIllegalStateException()
+	{
+		Example recentlyUsedList = new Example();
+		try
+		{
+			recentlyUsedList.RetrieveItem();
+			fail();
+		}
+		catch (IllegalStateException exception){
+			assertThat( exception.getMessage(), is("List is Empty"));
+		}
+	}
 }
